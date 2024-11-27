5677bf8cb3ba41deea4c5859b58f7c476755e100
Jakub Sycha <jsycha@protonmail.com>
2024-11-27 14:44:09 +0100

    feat: add info about project status

--- docs/intro/intro.md
+++ docs/intro/intro.md
@@ -4,6 +4,19 @@ sidebar_position: 1
 
 # Welcome to IronERP!
 
+:::warning
+
+IronERP is in a very early stages of development. Many features don't work yet and it is absolutely not
+production ready yet in any way.
+
+If you'd like to help us get to `v1.0`, feel free to [Contribute Code](https://github.com/IronERP/IronERP/blob/nightly/CONTRIBUTING.md)
+or join the <a href="https://ironerpcommunity.slack.com/archives/C07UUQP8LMP" class="slack-channel">Community Slack</a>.
+
+Please excuse the disorganized-ness and/or missing documentation, we're still new to this and we're figuring it out as we
+go along :)
+
+:::
+
 IronERP is an open-source ERP engine focused on flexibility and ease of use. Built with ASP.NET Core and MongoDB on the backend, and a Next.js frontend with TypeScript, IronERP provides a practical setup for managing business processes. It’s designed as an engine rather than just a framework, meaning it comes with a lot of ready-to-use features for small businesses while remaining highly customizable.
 
 A key component of IronERP is its graphical model builder, which lets users define models that form the foundation for entities throughout the system. Alongside this, a flexible CRUD engine dynamically generates views and forms for any database entity without requiring code, making it possible to adapt IronERP to a wide range of workflows with minimal setup.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,10 +39,10 @@ export default function Home(): JSX.Element {
         <section className=" dark:bg-gray-900">
           <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
             {/* New Release Callout */}
-            <a href="/blog/first-release" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
+            {/* <a href="/blog/first-release" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
               <span className="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">New!</span> <span className="text-sm font-medium">&#127881; IronERP v1.0.0 is out! See the changelog</span>
               <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-            </a>
+            </a> */}
             {/* End Release Callout */}
 
             {/* HERO */}
@@ -70,7 +70,7 @@ export default function Home(): JSX.Element {
 
 
             {/* Featured In Section */}
-            <div className="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
+            {/* <div className="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
               <span className="font-semibold text-gray-400 uppercase">FEATURED IN</span>
               <div className="flex flex-wrap justify-center items-center mt-8 text-gray-500 sm:justify-between">
                 <a href="#" className="mr-5 mb-5 lg:mb-0 hover:text-gray-800 dark:hover:text-gray-400">
@@ -102,7 +102,7 @@ export default function Home(): JSX.Element {
                   </svg>
                 </a>
               </div>
-            </div>
+            </div> */}
             {/* End Featured In Section */}
           </div>
         </section>
